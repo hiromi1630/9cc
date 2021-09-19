cd0d2fd9998b6256f852fb6fac54a3d3887db115
hiromi1630 <bigsea1630@docomo.ne.jp>
2021-09-19 21:35:18 +0900

    エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 // トークンの種類
 typedef enum
 {
@@ -53,7 +71,7 @@ bool consume(char op)
 void expect(char op)
 {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   token = token->next;
 }
 
@@ -62,7 +80,7 @@ void expect(char op)
 int expect_number()
 {
   if (token->kind != TK_NUM)
-    error("数ではありません");
+    error_at(token->str, "数ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -129,6 +147,7 @@ int main(int argc, char **argv)
 
   // トークナイズする
   token = tokenize(argv[1]);
+  user_input = argv[1];
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
