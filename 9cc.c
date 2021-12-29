4e21d3837134a7bf6dc81f0b2bf72ea5b8763241
hiromi1630 <bigsea1630@docomo.ne.jp>
2021-12-29 16:26:12 +0900

    比較演算子の実装

--- 9cc.c
+++ 9cc.c
@@ -5,46 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-//抽象構文木のノードの種類
-typedef enum
-{
-  ND_ADD, // +
-  ND_SUB, // -
-  ND_MUL, // *
-  ND_DIV, // /
-  ND_NUM, // 整数
-} NodeKind;
-
-typedef struct Node Node;
-
-//抽象構文木のノードの型
-struct Node
-{
-  NodeKind kind; // ノードの型
-  Node *lhs;     // 左辺
-  Node *rhs;     // 右辺
-  int val;       // kindがND_NUMの場合のみ使う
-};
-
-// ノードの作成
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
-{
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = kind;
-  node->lhs = lhs;
-  node->rhs = rhs;
-  return node;
-}
-
-// ノードの作成(整数)
-Node *new_node_num(int val)
-{
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = ND_NUM;
-  node->val = val;
-  return node;
-}
-
 // 入力プログラム
 char *user_input;
 
@@ -80,6 +40,7 @@ struct Token
   Token *next;    // 次の入力トークン
   int val;        // kindがTK_NUMの場合、その数値
   char *str;      // トークン文字列
+  int len;        // トークンの長さ
 };
 
 // 現在着目しているトークン
@@ -98,9 +59,9 @@ void error(char *fmt, ...)
 
 // 次のトークンが期待している記号のときには、トークンを1つ読み進めて
 // 真を返す。それ以外の場合には偽を返す。
-bool consume(char op)
+bool consume(char *op)
 {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
+  if (token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len))
     return false;
   token = token->next;
   return true;
@@ -108,10 +69,11 @@ bool consume(char op)
 
 // 次のトークンが期待している記号のときには、トークンを1つ読み進める。
 // それ以外の場合にはエラーを報告する。
-void expect(char op)
+void expect(char *op)
 {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
-    error_at(token->str, "'%c'ではありません", op);
+  if (token->kind != TK_RESERVED || strlen(op) != token->len ||
+      memcmp(token->str, op, token->len))
+    error_at(token->str, "\"%s\"ではありません", op);
   token = token->next;
 }
 
@@ -132,13 +94,19 @@ bool at_eof()
 }
 
 // 新しいトークンを作成してcurに繋げる
-Token *new_token(TokenKind kind, Token *cur, char *str)
+Token *new_token(TokenKind kind, Token *cur, char *str, int len)
 {
   Token *tok = calloc(1, sizeof(Token));
   tok->kind = kind;
   tok->str = str;
+  tok->len = len;
   cur->next = tok;
   return tok;
+}
+
+bool startswith(char *p, char *q)
+{
+  return memcmp(p, q, strlen(q)) == 0;
 }
 
 // 入力文字列pをトークナイズしてそれを返す
@@ -157,40 +125,148 @@ Token *tokenize(char *p)
       continue;
     }
 
-    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')')
+    if (startswith(p, "==") || startswith(p, "!=") || startswith(p, ">=") || startswith(p, "<="))
     {
-      cur = new_token(TK_RESERVED, cur, p++);
+      cur = new_token(TK_RESERVED, cur, p, 2);
+      p += 2;
+      continue;
+    }
+    if (strchr("+-*/()<>", *p))
+    {
+      cur = new_token(TK_RESERVED, cur, p++, 1);
       continue;
     }
     if (isdigit(*p))
     {
-      cur = new_token(TK_NUM, cur, p);
+      cur = new_token(TK_NUM, cur, p, 0);
+      char *q = p;
       cur->val = strtol(p, &p, 10);
+      cur->len = p - q;
       continue;
     }
 
     error_at(p, "トークナイズできません");
   }
 
-  new_token(TK_EOF, cur, p);
+  new_token(TK_EOF, cur, p, 0);
   return head.next;
 }
 
+//抽象構文木のノードの種類
+typedef enum
+{
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_EQ,  // ==
+  ND_NE,  // !=
+  ND_LT,  // <
+  ND_LE,  // <=
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+//抽象構文木のノードの型
+struct Node
+{
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kindがND_NUMの場合のみ使う
+};
+
+// ノードの作成
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+// ノードの作成(整数)
+Node *new_node_num(int val)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
 Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
 Node *mul();
 Node *unary();
 Node *primary();
 
 Node *expr()
 {
+  return equality();
+}
+
+Node *equality()
+{
+  Node *node = relational();
+  for (;;)
+  {
+    if (consume("=="))
+    {
+      node = new_node(ND_EQ, node, relational());
+    }
+    else if (consume("!="))
+    {
+      node = new_node(ND_NE, node, relational());
+    }
+    else
+    {
+      return node;
+    }
+  }
+}
+
+Node *relational()
+{
+  Node *node = add();
+  for (;;)
+  {
+    if (consume("<"))
+    {
+      node = new_node(ND_LT, node, add());
+    }
+    else if (consume("<="))
+    {
+      node = new_node(ND_LE, node, add());
+    }
+    else if (consume(">"))
+    {
+      node = new_node(ND_LT, add(), node);
+    }
+    else if (consume(">="))
+    {
+      node = new_node(ND_LE, add(), node);
+    }
+    else
+    {
+      return node;
+    }
+  }
+}
+
+Node *add()
+{
   Node *node = mul();
   for (;;)
   {
-    if (consume('+'))
+    if (consume("+"))
     {
       node = new_node(ND_ADD, node, mul());
     }
-    else if (consume('-'))
+    else if (consume("-"))
     {
       node = new_node(ND_SUB, node, mul());
     }
@@ -201,13 +277,33 @@ Node *expr()
   }
 }
 
+Node *mul()
+{
+  Node *node = unary();
+  for (;;)
+  {
+    if (consume("*"))
+    {
+      node = new_node(ND_MUL, node, unary());
+    }
+    else if (consume("/"))
+    {
+      node = new_node(ND_DIV, node, unary());
+    }
+    else
+    {
+      return node;
+    }
+  }
+}
+
 Node *unary()
 {
-  if (consume('+'))
+  if (consume("+"))
   {
     return unary();
   }
-  else if (consume('-'))
+  else if (consume("-"))
   {
     return new_node(ND_SUB, new_node_num(0), unary());
   }
@@ -219,33 +315,13 @@ Node *unary()
 
 Node *primary()
 {
-  if (consume('('))
+  if (consume("("))
   {
     Node *node = expr();
-    expect(')');
+    expect(")");
     return node;
   }
   return new_node_num(expect_number());
-}
-
-Node *mul()
-{
-  Node *node = unary();
-  for (;;)
-  {
-    if (consume('*'))
-    {
-      node = new_node(ND_MUL, node, unary());
-    }
-    else if (consume('/'))
-    {
-      node = new_node(ND_DIV, node, unary());
-    }
-    else
-    {
-      return node;
-    }
-  }
 }
 
 void gen(Node *node)
@@ -275,6 +351,26 @@ void gen(Node *node)
   case ND_DIV:
     printf("  cqo\n");
     printf("  idiv rax, rdi\n");
+    break;
+  case ND_EQ:
+    printf("  cmp rax, rdi\n");
+    printf("  sete al\n");
+    printf("  movzb rax, al\n");
+    break;
+  case ND_NE:
+    printf("  cmp rax, rdi\n");
+    printf("  setne al\n");
+    printf("  movzb rax, al\n");
+    break;
+  case ND_LT:
+    printf("  cmp rax, rdi\n");
+    printf("  setl al\n");
+    printf("  movzb rax, al\n");
+    break;
+  case ND_LE:
+    printf("  cmp rax, rdi\n");
+    printf("  setle al\n");
+    printf("  movzb rax, al\n");
     break;
   }
 
