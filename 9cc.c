cb90d0ebc4c0df93413f75755ed072afa169c368
hiromi1630 <bigsea1630@docomo.ne.jp>
2021-12-29 15:21:02 +0900

    致命的なバグの修正

--- 9cc.c
+++ 9cc.c
@@ -176,49 +176,9 @@ Token *tokenize(char *p)
   return head.next;
 }
 
-Node *primary()
-{
-  if (consume('('))
-  {
-    Node *node = expr();
-    expect(')');
-    return node;
-  }
-  return new_node_num(expect_number());
-}
-
-Node *unary()
-{
-  if (consume('+'))
-  {
-    return primary();
-  }
-  if (consume('-'))
-  {
-    return new_node(ND_SUB, new_node_num(0), primary());
-  }
-  return primary();
-}
-
-Node *mul()
-{
-  Node *node = unary();
-  for (;;)
-  {
-    if (consume('*'))
-    {
-      node = new_node(ND_MUL, node, unary());
-    }
-    else if (consume('/'))
-    {
-      node = new_node(ND_DIV, node, unary());
-    }
-    else
-    {
-      return node;
-    }
-  }
-}
+Node *expr();
+Node *mul();
+Node *primary();
 
 Node *expr()
 {
@@ -232,6 +192,37 @@ Node *expr()
     else if (consume('-'))
     {
       node = new_node(ND_SUB, node, mul());
+    }
+    else
+    {
+      return node;
+    }
+  }
+}
+
+Node *primary()
+{
+  if (consume('('))
+  {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
+}
+
+Node *mul()
+{
+  Node *node = primary();
+  for (;;)
+  {
+    if (consume('*'))
+    {
+      node = new_node(ND_MUL, node, primary());
+    }
+    else if (consume('/'))
+    {
+      node = new_node(ND_DIV, node, primary());
     }
     else
     {
